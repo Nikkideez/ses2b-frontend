759f8b5716774b34ceaf1c787b4e6477006f6b6d
Nikkideez <13769031@student.uts.edu.au>
2021-10-01 18:16:41 +1000

    Sorry Evan I crashed the build

--- components/dashComponents/studentExamRoom/VideoContainer.js
+++ components/dashComponents/studentExamRoom/VideoContainer.js
@@ -5,21 +5,21 @@ const StudentRTC = dynamic(() => import('./StudentRTC'), { ssr: false });
 
 
 export default function VideoContainer() {
-  const localRef = useRef();
-  const [stream, setStream] = useState();
-  const getVideo = async () => {
-    const localStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    setStream(localStream);
-    // localRef.current.srcObject = localStream;
-    console.log(stream);
-    console.log(localRef);
-  }
-  useEffect(() => {
-    getVideo();
-  }, [])
+  // const localRef = useRef();
+  // const [stream, setStream] = useState();
+  // const getVideo = async () => {
+  //   const localStream = await navigator.mediaDevices.getUserMedia({
+  //     video: true,
+  //     audio: true,
+  //   });
+  //   setStream(localStream);
+  //   // localRef.current.srcObject = localStream;
+  //   console.log(stream);
+  //   console.log(localRef);
+  // }
+  // useEffect(() => {
+  //   getVideo();
+  // }, [])
 
   return (
     <div>
