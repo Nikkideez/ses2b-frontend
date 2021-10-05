71733963f10a3873868d5bc3965a17779a822c9f
Mahmoud Daher <mahmoudhdaher98@gmail.com>
2021-10-06 08:44:20 +1100

    Update help.js

--- pages/dashboard/help.js
+++ pages/dashboard/help.js
@@ -7,8 +7,12 @@ export default function help() {
     return (
         <div>
             <SidebarV2>
-                <Typography variant="h5">
-                    This will display the Help section
+            <Typography variant="h5">
+                <div className={styles.container}>
+                   <div className={styles.centered}>Get in touch </div>
+                   <div className={styles.centeredemail}>https://www.uts.edu.au/current-students/managing-your-course/ask-uts/ask-uts</div>
+                   <div className={styles.centeredcontactnumber}>Monday - Friday, 9am - 5pm contact us on 1300 ASK UTS</div>
+                 </div>
                 </Typography>
             </SidebarV2>
         </div>
