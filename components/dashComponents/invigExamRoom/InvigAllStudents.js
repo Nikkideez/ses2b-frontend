035c9ce1862a5166d35e64a09ed511e6a494a317
Nikkideez <13769031@student.uts.edu.au>
2021-10-27 02:11:54 +1100

    floating buttons are all lined up on right for invig section

--- components/dashComponents/invigExamRoom/InvigAllStudents.js
+++ components/dashComponents/invigExamRoom/InvigAllStudents.js
@@ -6,17 +6,15 @@ import {
 import InvigContainer from './InvigContainer'
 import axios from 'axios';
 import AnnounceBtn from './AnnounceBtn';
+import Fab from '@material-ui/core/Fab';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 
-// const array = [
-//   { studentId: '123454', name: 'McLOVIN', image: "https://pbs.twimg.com/profile_images/1184543288801529862/Lw46ZMHU.jpg" },
-//   { studentId: '121212', name: 'Morpheus', image: "https://i.kym-cdn.com/entries/icons/facebook/000/009/889/Morpheus2.jpg" },
-//   { studentId: '303030', name: 'Scream Guy', image: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/31/1533133488-scary-movie-2000-ghost-faced-killer-wassup.jpg?resize=480:*" },
-//   { studentId: '404040', name: 'Squidward', image: "https://static.tvtropes.org/pmwiki/pub/images/b2a4ee66_8d4a_4345_bf45_c4f08e9eedd2.jpeg" },
-// ]
 const profilePic = [
   "https://pbs.twimg.com/profile_images/1184543288801529862/Lw46ZMHU.jpg",
   "https://i.kym-cdn.com/entries/icons/facebook/000/009/889/Morpheus2.jpg",
-  "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/31/1533133488-scary-movie-2000-ghost-faced-killer-wassup.jpg?resize=480:*", 
+  "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/31/1533133488-scary-movie-2000-ghost-faced-killer-wassup.jpg?resize=480:*",
   "https://static.tvtropes.org/pmwiki/pub/images/b2a4ee66_8d4a_4345_bf45_c4f08e9eedd2.jpeg"
 ]
 
@@ -26,6 +24,7 @@ export default function InvigAllStudents(props) {
   const [students, setStudents] = useState([])
   // const [strikes, setStrikes] = useState()
   const [exam, setExam] = useState()
+  const [isStarted, setIsStarted] = useState(false)
   // let students = [];
 
   // Getting firebase
@@ -63,25 +62,55 @@ export default function InvigAllStudents(props) {
     await updateDoc(doc(firestore, "exams", props.examID), {
       is_started: true
     })
+    setIsStarted(true)
   }
   //finish the exam
   const stopExam = async () => {
     await updateDoc(doc(firestore, "exams", props.examID), {
       is_started: false
     })
+    console.log(isStarted)
+    setIsStarted(false)
   }
-
-  // [[studentID, Warn], [studentID, Warn] ]
 
   // Button for toggling mute
   // Map function for every student who is in the exam room
   return (
     <div>
-      <AnnounceBtn examID={ props.examID }/>
-      <button onClick={() => startExam()}>Start Exam</button>
-      <button onClick={() => stopExam()}>Stop Exam</button>
-      <button onClick={() => setMute(!isMute)}>Mute Toggle</button>
-      <div style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-around'}}>
+      <AnnounceBtn examID={props.examID} />
+      {isStarted ?
+        <Fab color="primary" onClick={() => stopExam()}
+          style={{
+            margin: 0,
+            top: 'auto',
+            right: 30,
+            bottom: 90,
+            left: 'auto',
+            position: 'fixed',
+          }}><StopIcon /></Fab>
+        :
+        <Fab color="primary" onClick={() => startExam()}
+          style={{
+            margin: 0,
+            top: 'auto',
+            right: 30,
+            bottom: 90,
+            left: 'auto',
+            position: 'fixed',
+          }}
+        ><PlayArrowIcon/></Fab>
+      }
+      <Fab color="primary" onClick={() => setMute(!isMute)}
+        style={{
+          margin: 0,
+          top: 'auto',
+          right: 30,
+          bottom: 160,
+          left: 'auto',
+          position: 'fixed',
+        }}
+      ><VolumeUpIcon /></Fab>
+      <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-around' }}>
         {students.length > 0 ?
           students.map((student, index) =>
             <div key={index}>
@@ -92,19 +121,8 @@ export default function InvigAllStudents(props) {
                 subject={'MAT100'}
                 warnings={student[1]}
                 isMute={isMute}
-                token={ props.token }
-                />
-              {/* <InvigContainer
-                  studentId={student.student_id}
-                  image={profilePic[1]}
-                  name={student.first_name + " " + student.last}
-                  // rtc={new RTCPeerConnection(servers)}
-                  subject={'MAT100'}
-                  // localAudio={localAudio}
-                  isMute={isMute}
-                  examID={props.examID}
-                  warnings={exam.strikes[student.student_id]}
-                /> */}
+                token={props.token}
+              />
               <div style={{ padding: 10 }} />
             </div >)
           : null}
