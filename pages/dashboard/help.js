ec7b47ce00253cb7fa46d9c888c5f189355142e7
Anesu <anesu4@yahoo.com.au>
2021-10-26 22:01:51 +0800

    Update - Anesu: Updated Help Page with relevant links

--- pages/dashboard/help.js
+++ pages/dashboard/help.js
@@ -8,10 +8,10 @@ import { useRouter } from "next/router";
 
 export default function Help({ token }) {
     const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-	const [isStudent, setStudent] = useRecoilState(isStudentState);
+    const [isStudent, setStudent] = useRecoilState(isStudentState);
     const router = useRouter();
     useEffect(async () => {
-        if (!token){
+        if (!token) {
             alert("Please log in")
             router.push("/login")
         }
@@ -20,18 +20,49 @@ export default function Help({ token }) {
             setCurrentUser(user);
             setStudent(user.user_role === 2);
         }
-	}, []);
+    }, []);
+    var student_link = <a href={('https://www.uts.edu.au/current-students/managing-your-course/classes-and-assessment/exams')}>Learn More</a>;
+    // var student_text = <p>Are you taking an exam through ProctorUTS?</p>
+    var admin_link = <a href={('https://www.uts.edu.au/current-students/managing-your-course/classes-and-assessment/exams')}>Contact ProctorUTS</a>;
+    // var admin_text = <p>Are you ProctorUts's point of contact for your institution or department?</p>
+
     return (
         <div>
             <Sidebar>
                 <Typography variant="h5">
-                    This will display the Help section
+                    Help Section
                 </Typography>
+                <div className="container">
+                    <section className="section knowledge-base">
+                        <section className="categories blocks">
+                            <h1 className="jumbo-message" style={{ textAlign: 'center' }}> Need help? Click on the buttons below to get more information.</h1>
+                            <ul className="blocks-list">
+                                <li className="blocks-item">
+                                    <h2 className="blocks-item-title">Student</h2>
+                                    {/* <p className="blocks-item-description">Are you taking an exam through ProctorUTS?</p> */}
+                                    {/* <Typography >
+                                        Are you taking an exam through ProctorUTS? */}
+                                    <div className="blocks-item-link">{student_link}</div>
+                                    {/* </Typography> */}
+                                </li>
+
+                                <li className="blocks-item">
+                                    <h2 className="blocks-item-title">Administrator</h2>
+                                    {/* <p className="bloccks-item-description">Are you ProctorUts's point of contact for your institution or department?</p> */}
+                                    {/* <Typography>
+                                        Are you ProctorUts's point of contact for your institution or department? */}
+                                    <div className="blocks-item-link">{admin_link}</div>
+                                    {/* </Typography> */}
+                                </li>
+                            </ul>
+                        </section>
+                    </section>
+                </div>
             </Sidebar>
         </div>
     )
 }
 
 export function getServerSideProps({ req, res }) {
-  return { props: { token: req.cookies.token || "" } };
+    return { props: { token: req.cookies.token || "" } };
 }
